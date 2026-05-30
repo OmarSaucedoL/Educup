6a94fb4d1b6730c01c9a05aaf383c94551fbed3e
OmarSaucedoL <omarsaucedo000@gmail.com>
2026-05-30 03:12:05 -0400

    routes proteccion

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -4,22 +4,23 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('welcome');
+    return redirect('/login');
 })->name('home');
 
 Route::middleware(['auth'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+
+    require __DIR__.'/usuarios_routes.php';
+    require __DIR__.'/academico routes/cup_routes.php';
+    require __DIR__.'/academico routes/docentes_routes.php';
+    require __DIR__.'/academico routes/estudiantes_routes.php';
+    require __DIR__.'/academico routes/materias_routes.php';
+    require __DIR__.'/academico routes/horarios_routes.php';
+    require __DIR__.'/academico routes/aulas_routes.php';
+    require __DIR__.'/bitacora_routes.php';
 });
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
-require __DIR__.'/usuarios_routes.php';
-require __DIR__.'/academico routes/cup_routes.php';
-require __DIR__.'/academico routes/docentes_routes.php';
-require __DIR__.'/academico routes/estudiantes_routes.php';
-require __DIR__.'/academico routes/materias_routes.php';
-require __DIR__.'/academico routes/horarios_routes.php';
-require __DIR__.'/academico routes/aulas_routes.php';
-require __DIR__.'/bitacora_routes.php';
